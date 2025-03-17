d5b3e9e3072495e81d81c500fe8aa92d15085384
tamura <tomoki0927.7939@icloud.com>
2025-03-18 01:36:42 +0900

    feat: add models and db initialization (#2)

--- api/main.go
+++ api/main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/t2469/labor-management-system.git/models"
+	"log"
 	"net/http"
 )
 
 func main() {
+	models.InitDB()
+	if err := models.DB.AutoMigrate(&models.User{}); err != nil {
+		log.Fatalf("Failed to migrate database: %v", err)
+	}
+
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "Hello from Gin!"})
 	})
-	router.Run(":8080")
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatalf("Failed to run server: %v", err)
+	}
 }
---
+++ api/models/db.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"os"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tokyo", host, user, password, dbname, port)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	DB = db
+}
