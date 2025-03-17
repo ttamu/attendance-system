353f4e91f0d78d606eabce4fa6e5cf78b08069f6
tamura <tomoki0927.7939@icloud.com>
2025-03-18 02:28:21 +0900

    feat: set local timezone to Asia/Tokyo in main.go (#2)

--- api/main.go
+++ api/main.go
@@ -4,9 +4,16 @@ import (
 	"github.com/t2469/labor-management-system.git/models"
 	"github.com/t2469/labor-management-system.git/routes"
 	"log"
+	"time"
 )
 
 func main() {
+	loc, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		panic("failed to load location")
+	}
+	time.Local = loc
+
 	models.InitDB()
 	if err := models.DB.AutoMigrate(&models.User{}, &models.Attendance{}); err != nil {
 		log.Fatalf("Failed to migrate database: %v", err)
