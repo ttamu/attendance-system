a0fa94fc484fa1ec3a77cf4a70dbb9f7eefca458
tamura <tomoki0927.7939@icloud.com>
2025-03-20 23:58:55 +0900

    feat: add health_insurance_rate model (#6)

---
+++ api/models/health_insurance_rate.go
@@ -0,0 +1,15 @@
+package models
+
+import "gorm.io/gorm"
+
+type HealthInsuranceRate struct {
+	gorm.Model
+	PrefectureID        uint
+	Grade               string
+	MinMonthlyAmount    int
+	MaxMonthlyAmount    int
+	HealthTotalNonCare  float64
+	HealthHalfNonCare   float64
+	HealthTotalWithCare float64
+	HealthHalfWithCare  float64
+}
