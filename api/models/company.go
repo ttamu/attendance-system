54b57c480172e9450f191aede43745def47ae8fe
tamura <tomoki0927.7939@icloud.com>
2025-03-21 23:21:40 +0900

    feat: add Prefecture relation to Company model (#6)

--- api/models/company.go
+++ api/models/company.go
@@ -3,9 +3,10 @@ package models
 import "time"
 
 type Company struct {
-	ID                   uint                  `gorm:"primaryKey" json:"id"`
-	Name                 string                `json:"name"`
-	CreatedAt            time.Time             `json:"created_at"`
-	UpdatedAt            time.Time             `json:"updated_at"`
-	HealthInsuranceRates []HealthInsuranceRate `json:"health_insurance_rates" gorm:"foreignKey:CompanyID"`
+	ID           uint       `gorm:"primaryKey" json:"id"`
+	Name         string     `json:"name"`
+	PrefectureID uint       `json:"prefecture_id"`
+	Prefecture   Prefecture `json:"prefecture" gorm:"foreignKey:PrefectureID"`
+	CreatedAt    time.Time  `json:"created_at"`
+	UpdatedAt    time.Time  `json:"updated_at"`
 }
