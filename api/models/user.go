8fd32e46321fa23c650cea1839dfa331796e3c03
tamura <tomoki0927.7939@icloud.com>
2025-03-21 23:22:56 +0900

    feat: add company relation to user model (#6)

--- api/models/user.go
+++ api/models/user.go
@@ -5,6 +5,7 @@ import "time"
 type User struct {
 	ID            uint         `gorm:"primaryKey" json:"id"`
 	CompanyID     uint         `json:"company_id"`
+	Company       Company      `json:"company" gorm:"foreignKey:CompanyID"`
 	Name          string       `json:"name"`
 	MonthlySalary int          `json:"monthly_salary"`
 	DateOfBirth   time.Time    `json:"date_of_birth"`
