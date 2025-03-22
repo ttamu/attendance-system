ca4f693deb182af3d486ad103fb630bd0e10268f
tamura <tomoki0927.7939@icloud.com>
2025-03-22 23:59:05 +0900

    feat: add employee allowance model (#12)

---
+++ api/models/employee_allowance.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type EmployeeAllowance struct {
+	ID              uint          `gorm:"primaryKey" json:"id"`
+	EmployeeID      uint          `json:"employee_id"`
+	AllowanceTypeID uint          `json:"allowance_type_id"`
+	Amount          int           `json:"amount"`
+	CommissionRate  float64       `json:"commission_rate,omitempty"`
+	CreatedAt       time.Time     `json:"created_at"`
+	UpdatedAt       time.Time     `json:"updated_at"`
+	AllowanceType   AllowanceType `json:"allowance_type" gorm:"foreignKey:AllowanceTypeID"`
+}
