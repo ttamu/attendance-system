ecb3dd0587c4f6235d0b11bfaf96aaa9e9dc94a3
tamura <tomoki0927.7939@icloud.com>
2025-03-25 23:27:10 +0900

    feat: protect company and allowance routes with auth middleware (#20)

--- api/routes/allowances.go
+++ api/routes/allowances.go
@@ -3,10 +3,11 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/t2469/labor-management-system.git/controllers"
+	"github.com/t2469/labor-management-system.git/middleware"
 )
 
 func addAllowanceRoutes(router *gin.Engine) {
-	allowances := router.Group("/allowances")
+	allowances := router.Group("/allowances", middleware.AuthMiddleware())
 	{
 		allowances.POST("type", controllers.CrateAllowanceType)
 		allowances.POST("", controllers.CreateEmployeeAllowance)
