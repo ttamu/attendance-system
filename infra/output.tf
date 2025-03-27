f07e4072cc4f484b70300bb3f19d3a8b7c5ed496
tamura <tomoki0927.7939@icloud.com>
2025-03-27 20:01:22 +0900

    feat: add S3 and CloudFront for frontend hosting (#26)

--- infra/output.tf
+++ infra/output.tf
@@ -3,17 +3,22 @@ output "vpc_id" {
   value       = aws_vpc.main.id
 }
 
-# output "ecs_cluster_id" {
-#   description = "ID of the created ECS Cluster"
-#   value       = aws_ecs_cluster.main.id
-# }
-#
-# output "alb_dns_name" {
-#   description = "DNS name of the Application Load Balancer"
-#   value       = aws_lb.app_alb.dns_name
-# }
-#
-# output "rds_endpoint" {
-#   description = "Endpoint of the RDS instance"
-#   value       = aws_db_instance.db.address
-# }
+output "ecs_cluster_id" {
+  description = "ID of the created ECS Cluster"
+  value       = aws_ecs_cluster.api_cluster.id
+}
+
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = aws_lb.app_alb.dns_name
+}
+
+output "rds_endpoint" {
+  description = "Endpoint of the RDS instance"
+  value       = aws_db_instance.db.address
+}
+
+output "cloudfront_domain_name" {
+  description = "Domain name of the CloudFront Distribution"
+  value       = aws_cloudfront_distribution.frontend_distribution.domain_name
+}
---
+++ infra/s3_cloudfront.tf
@@ -0,0 +1,93 @@
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = "${var.project_name}-frontend"
+
+  tags = {
+    Name    = "${var.project_name}-frontend"
+    Project = var.project_name
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "frontend_bucket_website" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "OAI for ${var.project_name} frontend"
+}
+
+resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "GrantCloudFrontAccess",
+        Effect = "Allow",
+        Principal = {
+          AWS = aws_cloudfront_origin_access_identity.oai.iam_arn
+        },
+        Action   = "s3:GetObject",
+        Resource = "${aws_s3_bucket.frontend_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_cloudfront_distribution" "frontend_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.frontend_bucket.bucket_regional_domain_name
+    origin_id   = "S3-${aws_s3_bucket.frontend_bucket.id}"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "${var.project_name} frontend distribution"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods = ["GET", "HEAD", "OPTIONS"]
+    cached_methods = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = "S3-${aws_s3_bucket.frontend_bucket.id}"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+    minimum_protocol_version       = "TLSv1"
+  }
+
+  tags = {
+    Name    = "${var.project_name}-frontend"
+    Project = var.project_name
+  }
+}
