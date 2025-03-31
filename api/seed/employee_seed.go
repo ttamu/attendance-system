e52fd25f1a6e2e9f35ab0bdea2e8a05321c774c5
tamura <tomoki0927.7939@icloud.com>
2025-03-31 16:23:16 +0900

    chore: use FirstOrCreate to avoid duplicating employees (#42)

--- api/seed/employee_seed.go
+++ api/seed/employee_seed.go
@@ -9,26 +9,53 @@ import (
 )
 
 func SeedEmployees(db *gorm.DB) error {
+	var companyTokyo models.Company
+	if err := db.Where("name = ?", "株式会社東京").First(&companyTokyo).Error; err != nil {
+		log.Printf("failed to get company 株式会社東京: %v", err)
+		return err
+	}
+
+	var companyHokkaido models.Company
+	if err := db.Where("name = ?", "株式会社北海道").First(&companyHokkaido).Error; err != nil {
+		log.Printf("failed to get company 株式会社北海道: %v", err)
+		return err
+	}
+
 	employees := []models.Employee{
 		{
-			CompanyID:     1,
+			CompanyID:     companyTokyo.ID,
 			Name:          "山田太郎",
 			MonthlySalary: 300000,
 			DateOfBirth:   time.Date(1985, time.April, 15, 0, 0, 0, 0, time.UTC),
 		},
 		{
-			CompanyID:     2,
+			CompanyID:     companyHokkaido.ID,
 			Name:          "佐藤花子",
 			MonthlySalary: 280000,
 			DateOfBirth:   time.Date(1990, time.June, 10, 0, 0, 0, 0, time.UTC),
 		},
 	}
+
 	for _, emp := range employees {
-		if err := db.Create(&emp).Error; err != nil {
-			log.Printf("failed to create employee %s: %v", emp.Name, err)
-			return err
+		var employee models.Employee
+		result := db.Where(&models.Employee{
+			CompanyID: emp.CompanyID,
+			Name:      emp.Name,
+		}).
+			Attrs(models.Employee{
+				MonthlySalary: emp.MonthlySalary,
+				DateOfBirth:   emp.DateOfBirth,
+			}).
+			FirstOrCreate(&employee)
+		if result.Error != nil {
+			log.Printf("failed to create employee %s: %v", emp.Name, result.Error)
+			return result.Error
 		}
-		log.Println("Created employee", emp.Name)
+		if result.RowsAffected > 0 {
+			log.Println("Created employee", emp.Name)
+		} else {
+			log.Println("Employee already exists:", emp.Name)
+		}
 	}
 	return nil
 }
